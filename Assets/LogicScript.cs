a46e5f423e1fd79f31b7ee6f41c4fcb50fb64fa8
Betty Zhang <rzha402@aucklanduni.ac.nz>
2023-04-10 13:10:25 +1200

    create the bird and pipes, Next thing: UI

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
